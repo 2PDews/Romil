41fd4d39e65ec20e94203ac2b6cbb3b47085a97f
Glitch (knowing-onyx-sing) <none>
2025-02-17 06:15:05 +0000

    👀🍄 Checkpoint ./HTML/AddLeaguePlayers.html:74454399/14 ./CSS/AddLeaguePLayers.css copy:74454399/5195

--- JS/SelectFormat.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    const tournamentName = localStorage.getItem('tournamentName');
-    if (tournamentName) {
-      document.getElementById('tournamentTitle').innerText = tournamentName;  // Set the tournament name in the title
-    }
-  }
