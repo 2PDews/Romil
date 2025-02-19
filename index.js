682a785c726bb460ab9323b60d8b6aa04ea529bd
Glitch (standing-dandelion-talos) <none>
2025-02-19 04:30:39 +0000

    🐮🏝 Checkpoint ./index.js:74454399/1122 ./MainAddTeams.html:74454399/686 ./index.html:74454399/34

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-
 // Add click event listeners for the options
 document.getElementById('tournament-option').onclick = () => {
-    alert('Redirecting to Tournament Page'); // Replace with redirection logic
-    // window.location.href = "tournament.html"; // Uncomment to redirect
+    window.location.href = "Tournament.html";
 };
 
 document.getElementById('players-option').onclick = () => {
-    alert('Redirecting to Players Page'); // Replace with redirection logic
-    // window.location.href = "players.html"; // Uncomment to redirect
+    window.location.href = "MainAddPlayers.html";
 };
-
 
 document.getElementById('teams-option').onclick = () => {
-    alert('Redirecting to Teams Page'); // Replace with redirection logic
-    // window.location.href = "MainAddTeams.html"; // Uncomment to redirect
+    window.location.href = "MainAddTeams.html";
 };
-
-
