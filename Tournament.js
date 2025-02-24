7f0333b193c501a82fad48af7db7187d54ca2df2
Glitch (standing-dandelion-talos) <none>
2025-02-24 07:13:19 +0000

    👟🍬 Checkpoint ./Tournament.html:74454399/108 ./Tournament.js:74454399/142 ./index.html:74454399/2257 ./index.css:74454399/4423

--- Tournament.js
+++ Tournament.js
@@ -4,11 +4,6 @@ document.getElementById('create-tournament-btn').addEventListener('click', () =>
     window.location.href = 'CreateTournament.html'; // Redirect to Create Tournament page
 });
 
-document.getElementById('follow-tournament-btn').addEventListener('click', () => {
-    window.location.href = 'FollowTournament.html'; 
-});
-
-
 document.getElementById("saved-tournament-btn").addEventListener("click", function() {
     window.location.href = "SaveTournament.html";
 });
