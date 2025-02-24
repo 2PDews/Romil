9d94854229d53acf605f835f48211ca491a5237b
Glitch (standing-dandelion-talos) <none>
2025-02-24 05:00:38 +0000

    🎩🎗 Checkpoint ./index.html:74454399/12236 ./index.js:74454399/1072 ./index.css:74454399/6501

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ document.getElementById('players-option').onclick = () => {
 document.getElementById('teams-option').onclick = () => {
     window.location.href = "MainAddTeams.html";
 };
+
+document.getElementById('follow-option').onclick = () => {
+    window.location.href = "FollowTournament.html";
+};
+
+        function toggleMenu(event) {
+            event.stopPropagation(); // Prevents closing menu when clicking the menu icon
+            document.getElementById("sidebar").classList.toggle("active");
+        }
+
+        function closeMenu(event) {
+            let sidebar = document.getElementById("sidebar");
+            if (sidebar.classList.contains("active") && !sidebar.contains(event.target)) {
+                sidebar.classList.remove("active");
+            }
+        }
+
+        document.body.addEventListener("click", closeMenu); // Close sidebar on body click
